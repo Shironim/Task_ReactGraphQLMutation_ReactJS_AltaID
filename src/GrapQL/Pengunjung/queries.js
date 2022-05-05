443e2025d8d76aa5d7c69527abb5600dba1d4709
Shironim <dimasseto.id@gmail.com>
2022-05-05 10:28:53 +0700

    add task Grapql Mutation

---
+++ src/GrapQL/Pengunjung/queries.js
@@ -0,0 +1,77 @@
+import { gql } from '@apollo/client';
+
+export const GET_PENGUNJUNG = gql`
+query pengunjung {
+  task_grapql_pengunjung {
+    id
+    jenis_kelamin
+    nama
+    umur
+  }
+}
+`;
+
+export const GET_PENGUNJUNG_BY_ID = gql`
+query pengunjung($id: Int) {
+  task_grapql_pengunjung(where: {id: {_eq: $id}}) {
+    id
+    jenis_kelamin
+    nama
+    umur
+  }
+}
+`;
+
+export const DELETE_PENGUNJUNG_BY_ID = gql`
+mutation Pengunjung($id: Int) {
+  delete_task_grapql_pengunjung(where: {id: {_eq: $id}}) {
+    returning {
+      id
+      jenis_kelamin
+      nama
+      umur
+    }
+  }
+}
+`;
+
+export const INSERT_PENGUNJUNG = gql`
+mutation Pengunjung( 
+  $jenis_kelamin: String, 
+  $nama: String, 
+  $umur: Int) {
+  insert_task_grapql_pengunjung(
+    objects: {
+      jenis_kelamin: $jenis_kelamin, 
+      nama: $nama, 
+      umur: $umur}) {
+    returning {
+      id
+      jenis_kelamin
+      nama
+      umur
+    }
+  }
+}
+`;
+
+export const UPDATE_PENGUNJUNG_BY_ID = gql`
+mutation Pengunjung(
+  $id: Int,
+  $jenis_kelamin: String, 
+  $nama: String, 
+  $umur: Int ) {
+  update_task_grapql_pengunjung(
+    where: {id: {_eq: $id}}, 
+    _set: {
+      jenis_kelamin: $jenis_kelamin, 
+      nama: $nama, 
+      umur: $umur}) {
+    returning {
+      jenis_kelamin
+      nama
+      umur
+    }
+  }
+}
+`;
